3a3ddda7b2e7cf6bdcb351806c7eaa9d4f4e2cd8
MikuxII <mickaelpoupion@gmail.com>
2022-10-23 18:52:17 +0200

    update

--- UYW/src/main/java/MRUYW/UYW/repo/IRessourceRepository.java
+++ UYW/src/main/java/MRUYW/UYW/repo/IRessourceRepository.java
@@ -1,9 +1,14 @@
 package MRUYW.UYW.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import MRUYW.UYW.model.Ressource;
 
 public interface IRessourceRepository extends JpaRepository<Ressource, Integer> {
+    
+    // Récupère les 6 jeux pour la parti
+    public List<Ressource> findAll();
     
 }
